828b346c440774c0af0cf71b2ef524dec2fff6e1
DavidODW <supplice@gmail.com>
2023-12-20 13:27:20 +0100

    base template

--- header.php
+++ header.php
@@ -8,5 +8,9 @@
 </head>
 
 <body <?php body_class(); ?>>
-    
+    <header class="header">
+        <a href="<?php echo home_url( '/' ); ?>">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo">
+        </a>  
+    </header>
     <?php wp_body_open(); ?>
