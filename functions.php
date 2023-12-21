3e3c3d276d70a181e305811090199cd5ecd5cbaa
DavidODW <supplice@gmail.com>
2023-12-21 14:25:33 +0100

    style

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 function photographe_event_theme_assets() {
     wp_enqueue_script('jquery' );
     wp_enqueue_style('photographe_event', get_stylesheet_uri(), array(), '1.0');
-    wp_enqueue_style('photographe_event', get_template_directory_uri() . '/css/theme.css', array(), '1.0');
+    wp_enqueue_style('photographe_event', get_template_directory_uri() . '/css/style.css', array(), '1.0');
     wp_enqueue_script('photographe_event', get_template_directory_uri() . '/script/script.js', array(), null, true);
 }
 add_action('wp_enqueue_scripts', 'photographe_event_theme_assets');
