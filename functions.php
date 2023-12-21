03320852f0ba5feb7c7267e2b8181f327db0ede7
DavidODW <supplice@gmail.com>
2023-12-21 15:30:40 +0100

    menu

--- functions.php
+++ functions.php
@@ -1,9 +1,9 @@
 <?php
 function photographe_event_theme_assets() {
     wp_enqueue_script('jquery' );
-    wp_enqueue_style('photographe_event', get_stylesheet_uri(), array(), '1.0');
-    wp_enqueue_style('photographe_event', get_template_directory_uri() . '/css/style.css', array(), '1.0');
-    wp_enqueue_script('photographe_event', get_template_directory_uri() . '/script/script.js', array(), null, true);
+    wp_enqueue_style('photographe_styles', get_stylesheet_uri(), array(), '1.0');
+    wp_enqueue_style('photographe_theme', get_template_directory_uri() . '/css/theme.css', array(), '1.0');
+    wp_enqueue_script('photographe_script', get_template_directory_uri() . '/script/script.js', array(), null, true);
 }
 add_action('wp_enqueue_scripts', 'photographe_event_theme_assets');
 
@@ -14,9 +14,7 @@ function theme_setup() {
     add_theme_support('title-tag');
 
     register_nav_menus(array(
-        'navigation' => 'Menu Principal',
-        'container' => 'ul', 
-        'menu_class' => 'nav__menu', 
+        'main-menu' => 'Menu Principal',
     ));
 }
 add_action('after_setup_theme', 'theme_setup');
--- header.php
+++ header.php
@@ -12,7 +12,8 @@
             <img class="header__logo_img"src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo">
         </a>  
         <div class="nav__menu__wrapper">
-            <?php wp_nav_menu( array( 'theme_location' => 'main' ) ); ?>
+            <?php 
+            wp_nav_menu( array( 'theme_location' => 'main-menu','container' => 'ul','menu_class' => 'nav__menu', ) ); ?>
         </div>
     </header>
     <?php wp_body_open(); ?>
