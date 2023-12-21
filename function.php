357fcc5e7d87077d2fbc8bb62a2c6a477486f367
DavidODW <supplice@gmail.com>
2023-12-21 13:41:14 +0100

    function

--- function.php
+++ function.php
@@ -1,16 +1,26 @@
-<?php 
-
-// Ajouter la prise en charge des images mises en avant
-add_theme_support( 'post-thumbnails' );
-
-// Ajouter automatiquement le titre du site dans l'en-tête du site
-add_theme_support( 'title-tag' );
-
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-function theme_enqueue_styles() {
-    wp_enqueue_style('theme-style', get_template_directory_uri() . 'css/theme.css' );
+<?php
+function photographe_event_theme_assets() {
+    wp_enqueue_script('jquery' );
+    wp_enqueue_style('photographe_event', get_stylesheet_uri(), array(), '1.0');
+    wp_enqueue_style('photographe_event', get_template_directory_uri() . '/css/theme.css', array(), '1.0');
+    wp_enqueue_script('photographe_event', get_template_directory_uri() . '/script/script.js', array(), null, true);
 }
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_scripts' );
-function theme_enqueue_scripts() {
-    wp_enqueue_script('script', get_template_directory_uri() . '/scripts/menu.js');
-}  
+add_action('wp_enqueue_scripts', 'photographe_event_theme_assets');
+
+
+function theme_setup() {
+    add_theme_support('post-thumbnails');
+
+    add_theme_support('title-tag');
+
+    register_nav_menus(array(
+        'navigation' => 'Menu Principal',
+        'container' => 'ul', 
+        'menu_class' => 'nav__menu', 
+    ));
+}
+add_action('after_setup_theme', 'theme_setup');
+
+
+
+?>
--- header.php
+++ header.php
@@ -2,15 +2,17 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    
-    <?php wp_head(); ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>  
+    <?php wp_head();?>
 </head>
 
 <body <?php body_class(); ?>>
     <header class="header">
-        <a href="<?php echo home_url( '/' ); ?>">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo">
+        <a class="header__logo" href="<?php echo home_url( '/' ); ?>">
+            <img class="header__logo_img"src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo">
         </a>  
+        <div class="nav__menu__wrapper">
+            <?php wp_nav_menu( array( 'theme_location' => 'main' ) ); ?>
+        </div>
     </header>
     <?php wp_body_open(); ?>
