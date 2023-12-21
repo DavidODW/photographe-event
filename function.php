7e7193a42219e66982c1193629942c9eab3c7313
DavidODW <supplice@gmail.com>
2023-12-21 09:02:18 +0100

    modale template + sass + script

--- function.php
+++ function.php
@@ -5,3 +5,12 @@ add_theme_support( 'post-thumbnails' );
 
 // Ajouter automatiquement le titre du site dans l'en-tête du site
 add_theme_support( 'title-tag' );
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style('theme-style', get_template_directory_uri() . 'css/theme.css' );
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_scripts' );
+function theme_enqueue_scripts() {
+    wp_enqueue_script('script', get_template_directory_uri() . '/scripts/menu.js');
+}  
